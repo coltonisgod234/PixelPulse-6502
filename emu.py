11b7d078a3d67403bb20c29ae7d990b6544c3abe
Colton Wollinger <cwoll1home@gmail.com>
2024-04-28 08:58:05 -0400

    CPU Works Now, Still Not Quite Done Yet, I Would Say 1/2 Done Now

--- emu.py
+++ emu.py
@@ -1,4 +1,4 @@
-rom = "\x00\x00\x01\xFF" # Will Change To Argparse, Only Like This In Debug Mode.
+rom = "\x00\x00\x01\xFE" # Will Change To Argparse, Only Like This In Debug Mode.
 
 class BinaryRegister:
     """
@@ -11,7 +11,7 @@ class BinaryRegister:
     def change(self, value):
         if value > 2 ** self.bitwidth - 1:
             print("Register Overflow, Setting Overflow Flag And Resetting To 0")
-            reg_flags[0] = True
+            reg_flags.carry = True
             self.value = 0
         else:
             self.value = value
@@ -30,30 +30,32 @@ class ConstrainedRegister:
         if value > self.max:
             print("Register Overflow, Setting Overflow Flag And Resetting To 0")
             if self.allow_carry:
-                reg_flags[0] = True
+                reg_flags.carry = True
             self.value = 0
         elif value < self.min:
-            print("Register Underflow, Overflowing To 255")
-            self.value = 255
+            print("Register Underflow, Overflowing To Maximim")
+            self.value = self.max
         else:
             self.value = value
 
-def getRamValue(address):
-    """
-    Fetches A Value In Ram As A `BinaryRegister` Object
-    Example use:
-    `getRamObject(3000) = 255`
-    """
-    return ram[address].value
+class FlagsRegister:
+    def __init__(self, carry=False,equal=False,sign=False,res=False):
+        self.carry = carry
+        self.equal = equal
+        self.sign = sign
+        self.reserved = res
+        
 
 # Initalize The Registers
 reg_a = BinaryRegister()
 reg_x = BinaryRegister()
 reg_y = BinaryRegister()
-reg_flags = [False, False, False, False] # This One Is Special (♪freinds♪)
+reg_flags = FlagsRegister() # This One Is Special (♪freinds♪)
 #           Carry, Equal, Sign,  Reserved
 
 reg_programcounter = ConstrainedRegister(0, 32768, can_set_carry_flag=False)
+saved_pc = [ConstrainedRegister(0, 32768, can_set_carry_flag=False) for i in range(16)]
+saved_pc_count = ConstrainedRegister(0, 15)
 
 ram = [BinaryRegister() for i in range(32768)]
 
@@ -62,35 +64,126 @@ while reg_programcounter.value < len(rom):
     operand = rom[reg_programcounter.value + 1]
 
     if opcode == "\x00": # NOP
-        print("0x00 | NOP: Do Nothing")
+        pass # Do Nothing
     elif opcode == "\x01": # LDA #
-        print("0x01 | LDA #: Load accumulator with a value.")
-        reg_a.value = operand
+        reg_a.value = operand # Set Register A To The Operand
     elif opcode == "\x02": # LDY #
-        reg_y.value = operand
+        reg_y.value = operand # Set Register Y To The Operand
     elif opcode == "\x03": # LDX #
-        reg_x.value = operand
+        reg_x.value = operand # Set Register X To The Operand
     elif opcode == "\x04": # STA $
-        ram[operand].value = reg_a.value
+        ram[operand].value = reg_a.value # Set A Value In Memory To Register A
     elif opcode == "\x05": # STY $
-        ram[operand].value = reg_y.value
+        ram[operand].value = reg_y.value # Set A Value In Memory To Register Y
     elif opcode == "\x06": # STX $
-        ram[operand].value = reg_x.value
+        ram[operand].value = reg_x.value # Set A Value In Memory To Register X
     elif opcode == "\x07": # ADD $
-        reg_a.value = ram[operand].value + reg_a
+        reg_a.value = ram[operand].value + reg_a # Set The Value In Register A To The Sum Of A Value In RAM Plus Itself
     elif opcode == "\x08": # SUB $
-        reg_a.value = ram[operand].value - reg_a
+        reg_a.value = ram[operand].value - reg_a # Set The Value In Register A To The Subtraction Of A Value In RAM Minus Itself
     elif opcode == "\x09": # AND $
-        reg_a.value = ram[operand].value & reg_a
+        reg_a.value = ram[operand].value & reg_a # Set The Value In Register A To Itself Bitwise ANDed With A Value In RAM
     elif opcode == "\x0A": # ORA $
-        reg_a.value = ram[operand].value | reg_a
+        reg_a.value = ram[operand].value | reg_a # Set The Value In Register A To Itself Bitwise ORed With A Value In RAM
     elif opcode == "\x0B": # EOR $
-        reg_a.value = ram[operand].value ^ reg_a
-
-    # I'll Finish This Later.
+        reg_a.value = ram[operand].value ^ reg_a  # Set The Value In Register A To Itself Bitwise XORed With A Value In RAM
+    elif opcode == "\x0C": # CMP $
+        # Compare The Value In The A Register To A Value In RAM
+        if reg_a.value == ram[operand].value:
+            # Set The Equal Flag
+            reg_flags.equal = True
+        else: reg_flags.equal = False
+        
+        # See If It's Less Than (For BMI)
+        if reg_a.value < ram[operand].value:
+            # Set The Sign Flag
+            reg_flags.sign = True
+        else: reg_flags.sign = False
     
+    elif opcode == "\x0D": # CPX $
+        # Compare The Value In The X Register To A Value In RAM
+        if reg_x.value == ram[operand].value:
+            # Set The Equal Flag
+            reg_flags.equal = True
+        else: reg_flags.equal = False
+        
+        # See If It's Less Than (For BMI)
+        if reg_x.value < ram[operand].value:
+            # Set The Sign Flag
+            reg_flags.sign = True
+        else: reg_flags.sign = False
+    
+    elif opcode == "\x0E": # CPY $
+        # Compare The Value In The Y Register To A Value In RAM
+        if reg_y.value == ram[operand].value:
+            # Set The Equal Flag
+            reg_flags.equal = True
+        else: reg_flags.equal = False
+        
+        # See If It's Less Than (For BMI)
+        if reg_y.value < ram[operand].value:
+            # Set The Sign Flag
+            reg_flags.sign = True
+        else: reg_flags.sign = False
+    
+    elif opcode == "\x0F": # BEQ $
+        # Branch To A Routine If Equal Flag Is Set
+        if reg_flags.equal: 
+            saved_pc[saved_pc_count.value] = reg_programcounter.value
+            saved_pc_count.change(saved_pc_count.value + 1)
+            reg_programcounter = operand
+
+        reg_flags.equal = False
+
+    elif opcode == "\x10": # BNE $
+        # Branch To A Routine If Equal Flag Is Not Set
+        if not reg_flags.equal: 
+            saved_pc[saved_pc_count.value] = reg_programcounter.value
+            saved_pc_count.change(saved_pc_count.value + 1)
+            reg_programcounter = operand
+
+        reg_flags.equal = False
+    
+    elif opcode == "\x11": # BPS $
+        # Branch To A Routine If Sign Flag Is Not Set
+        if not reg_flags.sign:
+            saved_pc[saved_pc_count.value] = reg_programcounter.value
+            saved_pc_count.change(saved_pc_count.value + 1)
+            reg_programcounter = operand
+
+        reg_flags.sign = False
+
+    elif opcode == "\x12": # BMI $
+        # Branch To A Routine If Sign Flag Is Set
+        if reg_flags.sign:
+            saved_pc[saved_pc_count.value] = reg_programcounter.value
+            saved_pc_count.change(saved_pc_count.value + 1)
+            reg_programcounter = operand
+
+        reg_flags.sign = False
+
+    elif opcode == "\x13": # JMP $
+        saved_pc = reg_programcounter.value
+        saved_pc_count.change(saved_pc_count.value + 1)
+        reg_programcounter = operand
+
+    elif opcode == "\x14": # RET
+        saved_pc_count.change(saved_pc_count.value - 1)
+        reg_programcounter = saved_pc[saved_pc_count.value]
+
+    elif opcode == "\x15": # INT
+        print("INTERUPT REQUEST NOT HANDLED")
+
+    elif opcode == "\x16": # LDA $
+        reg_a.value = ram[operand].value # Set Register A To A Value In Memory
+
+    elif opcode == "\x17": # LDX $
+        reg_x.value = ram[operand].value # Set Register X To A Value In Memory
+
+    elif opcode == "\x18": # LDY $
+        reg_y.value = ram[operand].value # Set Register Y To A Value In Memory
 
     reg_programcounter.change(reg_programcounter.value + 2)
-    print(reg_programcounter.value)
+    print(f"PC:{reg_programcounter.value} | A:{reg_a.value} | X: {reg_x.value} | Y: {reg_y.value} | CARRY: {reg_flags.carry} | EQUAL: {reg_flags.equal} | SIGN: {reg_flags.sign}")
 
 print("Program Ended, No More Instructions To Run")
