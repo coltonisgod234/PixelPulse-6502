1ab670bb93f985da99f4d1864b4018d3e2ad2abf
Colton Wollinger <cwoll1home@gmail.com>
2024-04-29 16:45:28 -0400

    Something Is Broken With The FileNames. Let Me Fix It

--- emu/cpu.py
+++ emu/cpu.py
@@ -1,7 +1,6 @@
-import display
-import threading
+import pygame
 
-rom = "\x00\x00\x01\xFE" # Will Change To Argparse, Only Like This In Debug Mode.
+rom = [0x01,0xFF,0x00, 0x01,0x10,0x00] # Will Change To Argparse, Only Like This In Debug Mode.
 
 class BinaryRegister:
     """
@@ -14,7 +13,7 @@ class BinaryRegister:
     def change(self, value):
         if value > 2 ** self.bitwidth - 1:
             print("Register Overflow, Setting Overflow Flag And Resetting To 0")
-            reg_flags.carry = True
+            self.reg_flags.carry = True
             self.value = 0
         else:
             self.value = value
@@ -33,7 +32,7 @@ class ConstrainedRegister:
         if value > self.max:
             print("Register Overflow, Setting Overflow Flag And Resetting To 0")
             if self.allow_carry:
-                reg_flags.carry = True
+                self.reg_flags.carry = True
             self.value = 0
         elif value < self.min:
             print("Register Underflow, Overflowing To Maximim")
@@ -78,168 +77,192 @@ class GameController:
         self.pressed_as_int = pressed_int
         return pressed_int
 
-p1 = GameController()
-p2 = GameController()
+class cpu:
+    def __init__(self):
+        self.p1 = GameController()
+        self.p2 = GameController()
 
-# Initalize The Registers
-reg_a = BinaryRegister()
-reg_x = BinaryRegister()
-reg_y = BinaryRegister()
-reg_flags = FlagsRegister() # This One Is Special (♪freinds♪)
+        # Initalize The Registers
+        self.reg_a = BinaryRegister()
+        self.reg_x = BinaryRegister()
+        self.reg_y = BinaryRegister()
+        self.reg_flags = FlagsRegister() # This One Is Special (♪freinds♪)
 
-reg_programcounter = ConstrainedRegister(0, 32768, can_set_carry_flag=False)
-saved_pc = [ConstrainedRegister(0, 32768, can_set_carry_flag=False) for i in range(16)]
-saved_pc_count = ConstrainedRegister(0, 15)
+        self.reg_programcounter = ConstrainedRegister(0, 32768, can_set_carry_flag=False)
+        self.saved_pc = [ConstrainedRegister(0, 32768, can_set_carry_flag=False) for i in range(16)]
+        self.saved_pc_count = ConstrainedRegister(0, 15)
 
-ram = [BinaryRegister() for i in range(32768)]
+        self.ram = [BinaryRegister() for i in range(32768)]
 
-print("*** CPU IS OKAY ***")
+        print("*** CPU IS OKAY ***")
 
-print("*** RAM IS OKAY ***")
+        print("*** RAM IS OKAY ***")
 
-print("*** INITING DISPLAY ***")
+    def write_mem(self, address, data):
+        try:
+            self.ram[address].change(data.value)
+        except IndexError:
+            print("Invalid Or Bad Address", address)
 
-def tkinter_thread(threading_ready_event):
-    pixelPulseDisplayInstance = display.PixelPulseDisplay()
-    threading_ready_event.set() # It's Safe You Can Continue Now
-    pixelPulseDisplayInstance.root.mainloop()
+    def read_mem(self, address):
+        try:
+            return self.ram[address].value
+        except IndexError:
+            print("Invalid Or Bad Address", address)
 
-# Initalize The Display
-display_ready_event = threading.Event()
+    def combine_address(self, high_byte, low_byte):
+        # Combine the high byte and low byte into a 16-bit address
+        address = (high_byte << 8) | low_byte
+        return address
 
-pixelPulseDisplayInstanceThread = threading.Thread(target=tkinter_thread, args=(display_ready_event,))
-pixelPulseDisplayInstanceThread.start()
+    def run_once(self, rom):
+        opcode = rom[self.reg_programcounter.value]
+        operand = rom[self.reg_programcounter.value + 1]
+        address = self.combine_address(rom[self.reg_programcounter.value + 1], rom[self.reg_programcounter.value + 2])
+        #print(opcode, operand)
 
-# Now We Wait Around Until It's ready
-display_ready_event.wait()
-
-while reg_programcounter.value < len(rom):
-    opcode = rom[reg_programcounter.value]
-    operand = rom[reg_programcounter.value + 1]
-
-    if opcode == "\x00": # NOP
-        pass # Do Nothing
-    elif opcode == "\x01": # LDA #
-        reg_a.value = operand # Set Register A To The Operand
-    elif opcode == "\x02": # LDY #
-        reg_y.value = operand # Set Register Y To The Operand
-    elif opcode == "\x03": # LDX #
-        reg_x.value = operand # Set Register X To The Operand
-    elif opcode == "\x04": # STA $
-        ram[operand].value = reg_a.value # Set A Value In Memory To Register A
-    elif opcode == "\x05": # STY $
-        ram[operand].value = reg_y.value # Set A Value In Memory To Register Y
-    elif opcode == "\x06": # STX $
-        ram[operand].value = reg_x.value # Set A Value In Memory To Register X
-    elif opcode == "\x07": # ADD $
-        reg_a.value = ram[operand].value + reg_a # Set The Value In Register A To The Sum Of A Value In RAM Plus Itself
-    elif opcode == "\x08": # SUB $
-        reg_a.value = ram[operand].value - reg_a # Set The Value In Register A To The Subtraction Of A Value In RAM Minus Itself
-    elif opcode == "\x09": # AND $
-        reg_a.value = ram[operand].value & reg_a # Set The Value In Register A To Itself Bitwise ANDed With A Value In RAM
-    elif opcode == "\x0A": # ORA $
-        reg_a.value = ram[operand].value | reg_a # Set The Value In Register A To Itself Bitwise ORed With A Value In RAM
-    elif opcode == "\x0B": # EOR $
-        reg_a.value = ram[operand].value ^ reg_a  # Set The Value In Register A To Itself Bitwise XORed With A Value In RAM
-    elif opcode == "\x0C": # CMP $
-        # Compare The Value In The A Register To A Value In RAM
-        if reg_a.value == ram[operand].value:
-            # Set The Equal Flag
-            reg_flags.equal = True
-        else: reg_flags.equal = False
+        if opcode == 0x00: # NOP
+            pass # Do Nothing
+        elif opcode == 0x01: # LDA #
+            self.reg_a.value = operand # Set Register A To The Operand
+        elif opcode == 0x02: # LDY #
+            self.reg_y.value = operand # Set Register Y To The Operand
+        elif opcode == 0x03: # LDX #
+            self.reg_x.value = operand # Set Register X To The Operand
+        elif opcode == 0x04: # STA $
+            self.write_mem(address, self.reg_a)
+        elif opcode == 0x05: # STY $
+            self.write_mem(address, self.reg_y)
+        elif opcode == 0x06: # STX $
+            self.write_mem(address, self.reg_x)
+        elif opcode == 0x07: # ADD $
+            self.reg_a.value = self.read_mem(address) + self.reg_a # Set The Value In Register A To The Sum Of A Value In RAM Plus Itself
+        elif opcode == 0x08: # SUB $
+            self.reg_a.value = self.read_mem(address) - self.reg_a # Set The Value In Register A To The Subtraction Of A Value In RAM Minus Itself
+        elif opcode == 0x09: # AND $
+            self.reg_a.value = self.read_mem(address) & self.reg_a # Set The Value In Register A To Itself Bitwise ANDed With A Value In RAM
+        elif opcode == 0x0A: # ORA $
+            self.reg_a.value = self.read_mem(address) | self.reg_a # Set The Value In Register A To Itself Bitwise ORed With A Value In RAM
+        elif opcode == 0x0B: # EOR $
+            self.reg_a.value = self.read_mem(address) ^ self.reg_a  # Set The Value In Register A To Itself Bitwise XORed With A Value In RAM
+        elif opcode == 0x0C: # CMP $
+            # Compare The Value In The A Register To A Value In RAM
+            if self.reg_a.value == self.read_mem(address):
+                # Set The Equal Flag
+                self.reg_flags.equal = True
+            else: self.reg_flags.equal = False
+            
+            # See If It's Less Than (For BMI)
+            if self.reg_a.value < self.read_mem(address):
+                # Set The Sign Flag
+                self.reg_flags.sign = True
+            else: self.reg_flags.sign = False
         
-        # See If It's Less Than (For BMI)
-        if reg_a.value < ram[operand].value:
-            # Set The Sign Flag
-            reg_flags.sign = True
-        else: reg_flags.sign = False
-    
-    elif opcode == "\x0D": # CPX $
-        # Compare The Value In The X Register To A Value In RAM
-        if reg_x.value == ram[operand].value:
-            # Set The Equal Flag
-            reg_flags.equal = True
-        else: reg_flags.equal = False
+        elif opcode == 0x0D: # CPX $
+            # Compare The Value In The X Register To A Value In RAM
+            if self.reg_x.value == self.read_mem(address):
+                # Set The Equal Flag
+                self.reg_flags.equal = True
+            else: self.reg_flags.equal = False
+            
+            # See If It's Less Than (For BMI)
+            if self.reg_x.value < self.read_mem(address):
+                # Set The Sign Flag
+                self.reg_flags.sign = True
+            else: 
+                self.reg_flags.sign = False
         
-        # See If It's Less Than (For BMI)
-        if reg_x.value < ram[operand].value:
-            # Set The Sign Flag
-            reg_flags.sign = True
-        else: reg_flags.sign = False
-    
-    elif opcode == "\x0E": # CPY $
-        # Compare The Value In The Y Register To A Value In RAM
-        if reg_y.value == ram[operand].value:
-            # Set The Equal Flag
-            reg_flags.equal = True
-        else: reg_flags.equal = False
+        elif opcode == 0x0E: # CPY $
+            # Compare The Value In The Y Register To A Value In RAM
+            if self.reg_y.value == self.read_mem(address):
+                # Set The Equal Flag
+                self.reg_flags.equal = True
+            else: 
+                self.reg_flags.equal = False
+            
+            # See If It's Less Than (For BMI)
+            if self.reg_y.value < self.read_mem(address):
+                # Set The Sign Flag
+                self.reg_flags.sign = True
+            else: 
+                self.reg_flags.sign = False
         
-        # See If It's Less Than (For BMI)
-        if reg_y.value < ram[operand].value:
-            # Set The Sign Flag
-            reg_flags.sign = True
-        else: reg_flags.sign = False
+        elif opcode == 0x0F: # BEQ $
+            # Branch To A Routine If Equal Flag Is Set
+            if self.reg_flags.equal:
+                self.reg_programcounter = address
+            
+            else:
+                self.reg_flags.equal = False
+
+        elif opcode == 0x10: # BNE $
+            # Branch To A Routine If Equal Flag Is Not Set
+            if not self.reg_flags.equal: 
+                self.reg_programcounter = address
+
+            else:
+                self.reg_flags.equal = False
+        
+        elif opcode == 0x11: # BPS $
+            # Branch To A Routine If Sign Flag Is Not Set
+            if not self.reg_flags.sign:
+                self.reg_programcounter = address
+
+            else:
+                self.reg_flags.sign = False
+
+        elif opcode == 0x12: # BMI $
+            # Branch To A Routine If Sign Flag Is Set
+            if self.reg_flags.sign:
+                self.reg_programcounter = address
+
+            else:
+                self.reg_flags.sign = False
+
+        elif opcode == 0x13: # JMP $
+            self.reg_programcounter = operand
+
+        elif opcode == 0x14: # SPC
+            self.write_mem(0x1A20, self.reg_programcounter.value)
+            print("SAVED PROGRAM COUNTER AT 0x1A20")
+
+        elif opcode == 0x15: # INT
+            print("INTERUPT REQUEST NOT HANDLED")
+
+        elif opcode == 0x16: # LDA $
+            self.reg_a.value = self.ram[operand].value # Set Register A To A Value In Memory
+
+        elif opcode == 0x17: # LDX $
+            self.reg_x.value = self.ram[operand].value # Set Register X To A Value In Memory
+
+        elif opcode == 0x18: # LDY $
+            self.reg_y.value = self.ram[operand].value # Set Register Y To A Value In Memory
+
+        elif opcode == 0x19: # LPC
+            self.reg_programcounter.change(self.read_mem(0x1A20))
+
+        print(f"PC:{self.reg_programcounter.value} | A:{self.reg_a.value} | X: {self.reg_x.value} | Y: {self.reg_y.value} | CARRY: {self.reg_flags.carry} | EQUAL: {self.reg_flags.equal} | SIGN: {self.reg_flags.sign}")
+        self.reg_programcounter.change(self.reg_programcounter.value + 3)
+
+# Initialize Pygame
+pygame.init()
+# Set up Pygame window
+screen = pygame.display.set_mode((160, 240))
+pygame.display.set_caption("PixelPulse 6502 Emulator")
+
+print("********************* BOOTUP *************************")
+print("* PIXELPULSE 6502 IS A FANTASY VIDEO GAME CONSOLE    *")
+print("* THANK YOU TO __________ ON DISCORD AND THE PYGAME  *")
+print("* PYGAME TEAM FOR HELPING MAKE THIS PROJECT POSSIBLE!*")
+print("******************************************************")
+
+emulated_cpu = cpu()
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: running = False
     
-    elif opcode == "\x0F": # BEQ $
-        # Branch To A Routine If Equal Flag Is Set
-        if reg_flags.equal: 
-            saved_pc[saved_pc_count.value] = reg_programcounter.value
-            saved_pc_count.change(saved_pc_count.value + 1)
-            reg_programcounter = operand
-
-        reg_flags.equal = False
-
-    elif opcode == "\x10": # BNE $
-        # Branch To A Routine If Equal Flag Is Not Set
-        if not reg_flags.equal: 
-            saved_pc[saved_pc_count.value] = reg_programcounter.value
-            saved_pc_count.change(saved_pc_count.value + 1)
-            reg_programcounter = operand
-
-        reg_flags.equal = False
-    
-    elif opcode == "\x11": # BPS $
-        # Branch To A Routine If Sign Flag Is Not Set
-        if not reg_flags.sign:
-            saved_pc[saved_pc_count.value] = reg_programcounter.value
-            saved_pc_count.change(saved_pc_count.value + 1)
-            reg_programcounter = operand
-
-        reg_flags.sign = False
-
-    elif opcode == "\x12": # BMI $
-        # Branch To A Routine If Sign Flag Is Set
-        if reg_flags.sign:
-            saved_pc[saved_pc_count.value] = reg_programcounter.value
-            saved_pc_count.change(saved_pc_count.value + 1)
-            reg_programcounter = operand
-
-        reg_flags.sign = False
-
-    elif opcode == "\x13": # JMP $
-        saved_pc = reg_programcounter.value
-        saved_pc_count.change(saved_pc_count.value + 1)
-        reg_programcounter = operand
-
-    elif opcode == "\x14": # RET
-        saved_pc_count.change(saved_pc_count.value - 1)
-        reg_programcounter = saved_pc[saved_pc_count.value]
-
-    elif opcode == "\x15": # INT
-        print("INTERUPT REQUEST NOT HANDLED")
-
-    elif opcode == "\x16": # LDA $
-        reg_a.value = ram[operand].value # Set Register A To A Value In Memory
-
-    elif opcode == "\x17": # LDX $
-        reg_x.value = ram[operand].value # Set Register X To A Value In Memory
-
-    elif opcode == "\x18": # LDY $
-        reg_y.value = ram[operand].value # Set Register Y To A Value In Memory
-
-    print(f"PC:{reg_programcounter.value} | A:{reg_a.value} | X: {reg_x.value} | Y: {reg_y.value} | CARRY: {reg_flags.carry} | EQUAL: {reg_flags.equal} | SIGN: {reg_flags.sign}")
-    reg_programcounter.change(reg_programcounter.value + 2)
-    
-
-print("Program Ended, No More Instructions To Run")
-pixelPulseDisplayInstanceThread.join()
+    if emulated_cpu.reg_programcounter.value > len(rom) - 2: running = False
+    else: emulated_cpu.run_once(rom)
